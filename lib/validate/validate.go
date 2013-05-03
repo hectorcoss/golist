f5d0ba04e1339a2189ae425f408fc25dd7546694
Hector Coss <hector.coss@hotmail.com>
2013-05-02 22:41:46 -0500

    add lib

---
+++ lib/validate/validate.go
@@ -0,0 +1,10 @@
+// validate.go
+// Validate the form input using regex
+package validate
+
+//import "regex"
+
+func ValidateString(s *string) {
+	//regexString := "^[A-Z]{1,2}[a-z]{1,10}"
+	*s = "String"
+}
